5bd75e476c1187cfb56723dbdd71431aac06e835
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-03-22 04:36:01 +0500

    update script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable max-classes-per-file */
+
 const form = document.querySelector('[data-form]');
 const section = document.querySelector('[data-author-book]');
 const author = document.querySelector('[data-author-input]');
