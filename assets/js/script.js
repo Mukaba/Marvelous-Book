a5c143cca10c49e5ab8fa62f51b71f5920393494
Kitenge Mukaba Jean-louis <jeanlouismukaba01@yahoo.com>
2023-03-21 20:23:52 +0100

    Add class to remove books

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,46 +5,61 @@ const book = document.querySelector('[data-book-input]');
 
 // create an arry to hold all books
 let library = [];
+// display data into html
+class display {
+  static displayData() {
+    const displayData = library.map((item) => `
+            <div class="author-books">
+              <h4 class="author-name">${item.inputVal}</h4>
+              <h5 class="boock-name">${item.inputVall}</h5>
+              <small class="del delete" data-id = ${item.id}>Delete</small>
+              <hr>
+            </div>
+      `);
+    // inject the data into html
+    section.innerHTML = (displayData).join('');
+  }
 
-// prevent auto refresh when clickimg to the submit button
-// generate a random ID to be assign to authors_books
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  let id = Math.random() * 500;
-  const inputVal = new InputVal(id, author.value, book.value);
-  library = [...library, inputVal];
-  // console.log(library);
-  display.displayData();
-  display.restoreInput()
-});
+  static restoreInput() {
+    author.value = '';
+    book.value = '';
+  }
 
-// objeck instance
+  static deleteFromStore() {
+    section.addEventListener('click', (e) => {
+      if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+      }
+      const checkId = e.target.dataset.id;
+      // delete fully from the native array
+      display.deleteFromArray(checkId);
+    });
+  }
+
+  static deleteFromArray(id) {
+    library = library.filter((item) => item.id !== +id);
+  }
+}
+
+// instance
 class InputVal {
-  constructor(id, inputVal){
+  constructor(id, inputVal) {
     this.id = id;
     this.inputVal = inputVal;
     this.inputVall = book.value;
   }
 }
-// display data into html
-class display {
-  static displayData(){
-    let displayData = library.map((item) => {
-      return `
-            <div class="author-books">
-              <h4 class="author-name">${item.inputVal}</h4>
-              <h5 class="boock-name">${item.inputVall}</h5>
-              <small class="delete">Delete</small>
-              <hr>
-            </div>
-      `
-    });
-    // inject the data into html
-    section.innerHTML = (displayData).join("");
-  }
-  static restoreInput() {
-    author.value = "";
-    book.value = "";
-  }
-}
 
+// prevent auto refresh when clickimg to the submit button
+// generate a random ID to be assign to authors_books
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const id = Math.random() * 500;
+  const inputVal = new InputVal(id, author.value, book.value);
+  library = [...library, inputVal];
+  // console.log(library);
+  display.displayData();
+  display.restoreInput();
+  // delete book-author
+  display.deleteFromStore();
+});
