a5e77a7f41423b8bbc8fd0d5368f8ea1997ef2d3
Kitenge Mukaba Jean-louis <jeanlouismukaba01@yahoo.com>
2023-03-28 01:03:25 +0100

    Remove datastorage class to a module

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,12 @@
+import navLinks from './modules/nav.js';
+import keepdata from './modules/datastorage.js';
+
+navLinks();
+
 const form = document.querySelector('[data-form]');
 const section = document.querySelector('[data-author-book]');
 const author = document.querySelector('[data-author-input]');
 const book = document.querySelector('[data-book-input]');
-
-// store
-class keepdata {
-  static addToLocalStorage(library) {
-    const storage = localStorage.setItem('inputVal', JSON.stringify(library));
-    return storage;
-  }
-
-  static getStorage() {
-    const storage = localStorage.getItem('inputVal') == null
-      ? [] : JSON.parse(localStorage.getItem('inputVal'));
-    return storage;
-  }
-}
 
 // create an arry to hold all books
 let library = keepdata.getStorage();
